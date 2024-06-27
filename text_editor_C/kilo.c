9a1e367ced64e7033965266984565d58b0c2b3f3
kurision <kumalanil34@gmail.com>
2024-06-27 23:50:03 +0545

    step-34

--- text_editor_C/kilo.c
+++ text_editor_C/kilo.c
@@ -56,24 +56,25 @@ char editorReadKey(){
 }
 
 int getCursorPosition(int *rows,int *cols){
+	char buf[32];
+	unsigned int i = 0;
+
 	if(write(STDOUT_FILENO,"\x1b[6n",4)!=4) return -1;
 
-	printf("\r\n");
-	char c;
-	while(read(STDIN_FILENO,&c,1)==1){
-		if(iscntrl(c)){
-			printf("%d\r\n",c);
-		}else{
-			printf("%d ('%c')\r\n",c,c);
-		}
-
+	while(i<sizeof(buf)-1){
+		if(read(STDIN_FILENO,&buf[i],1)!=1) break;
+		if(buf[i]=='R') break;
+		i++;
 	}
-	editorReadKey();
-	return -1;
+	buf[i]=0;
+	
+	if(buf[0]!='\x1b' || buf[1]!= '[') return -1;
+	if(sscanf(&buf[2],"%d;%d",rows,cols)!=2) return -1;
+	return 0;
 }
 int getWindowSize(int *rows,int *cols){
 	struct winsize ws;
-	if(1 || ioctl(STDIN_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0){
+	if(ioctl(STDIN_FILENO,TIOCGWINSZ,&ws)==-1 || ws.ws_col == 0){
 		if(write(STDIN_FILENO,"\x1b[999C\x1b[999B",12)!=12) return -1;
 		return getCursorPosition(rows,cols);
 	}else{
